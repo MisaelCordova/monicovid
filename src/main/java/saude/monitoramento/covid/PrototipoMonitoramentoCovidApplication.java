0e6ad5ba9a25357a0ede796adf596b7942092764
Misael Cordova <misael.cordova.f@gmail.com>
2021-01-20 12:41:30 -0300

    Initial Commit

---
+++ src/main/java/saude/monitoramento/covid/PrototipoMonitoramentoCovidApplication.java
@@ -0,0 +1,13 @@
+package saude.monitoramento.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipoMonitoramentoCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipoMonitoramentoCovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saude/monitoramento/covid/PrototipoMonitoramentoCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package saude.monitoramento.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoMonitoramentoCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
