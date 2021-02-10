8cff3f78b2877d67c69fc4f98f4c7dbe44c10c66
Misael Cordova <misael.cordova.f@gmail.com>
2021-02-10 16:54:43 -0300

    feat:model-paciente

---
+++ src/main/java/saude/monitoramento/covid/model/Paciente.java
@@ -0,0 +1,18 @@
+package saude.monitoramento.covid.model;
+
+import java.util.Date;
+
+public class Paciente {
+	int paciente;
+	boolean temCPF;
+	int CNS;
+    Date dateNasc;
+    String nome;
+    String bairro;
+    int telefone;
+    boolean profisSaude;
+    int CPF;
+    String Nomemae;
+    char sexo;
+	
+}
